ccfc265b6bb25df52ce08868e7a13e41cf1f09b4
Sena <inabacker24@gmail.com>
2017-10-20 21:58:48 -0700

    Added a file till 4

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// comment
+# comment
+/*
+comment
+comment
+*/
+
+$msg = "Hello from the TOP!"; //変数に代入
+echo $msg; //呼び出し
+var_dump($msg); //データ型と文字数を表示
+ ?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<body>
+  <p>Hello World <?php echo " from PHP"; ?></p>
+</body>
+</html> -->
+
+<!-- <?= " from PHP"; ?>でもOK -->
